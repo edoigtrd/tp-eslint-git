f3632b69a2da9c9473f4c6f290185c3da4439c3b
edoigtrd <edoigtrd@proton.me>
2025-04-16 16:10:06 +0200

    Ajout de GitHub Actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,32 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import eslintPluginImport from 'eslint-plugin-import'
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+
+	  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      'no-console': 'warn',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+    },
+  }
 ]);
+
